65782c2c23956c4f93c64ae4292bb2e41bd9dc56
Ginger Williams <gingerwilliams@MacBook-Pro.local>
2019-02-25 17:42:42 -0500

    lifted index.js to root

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-    entry: "./src/index.js",
+    entry: "./index.js",
     output: {
         filename: "index.js",
         publicPath: "/dist/",

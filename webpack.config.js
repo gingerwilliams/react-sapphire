5570cd5b4736feb7cc331a3210e92377292424b2
Ginger Williams <gingerwilliams@MacBook-Pro.local>
2019-02-26 09:48:43 -0500

    updated webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -66,5 +66,8 @@ module.exports = {
     },
     resolve: {
         extensions: [".js", ".jsx"]
+    },
+    externals: {
+        react: "commonjs react" // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
     }
 };
